44b2c9f42df020a6001c7f5ae6059c2681c8aa3f
Muad2003 <anuphap2003118@gmail.com>
2023-07-26 16:01:12 +0700

    update homework 3-2

--- Homework/HW03/hw03-2-codex-and-fix-bug.cpp
+++ Homework/HW03/hw03-2-codex-and-fix-bug.cpp
@@ -3,22 +3,22 @@
 #include <stdbool.h>
 
 int main() {
-    float a;
-    float b = 3;
-    bool _1 = false;
-    printf("%d ", _1);
+    float a ;
+    float b = 3 ;
+    bool _1 = false ;
+    printf( "%d ", _1 ) ;
 
-    int i = 0;
-    if (_1)
-        printf("ok ");
+    int i = 0 ;
+    if ( _1 )
+        printf( "ok " ) ;
     else 
-        while (i <= 5)
-            printf("not-okey-%d ", ++i);
+        while ( i <= 5 )
+            printf( "not-okey-%d ", ++i ) ;
 
-    for (i = 3; i < 10; i += 1) {
-        if (i % 2 == (!_1 ? 1 : 0))
-            printf("see see ");
+    for ( i = 3 ; i < 10 ; i += 1 ) {
+        if ( i % 2 == ( !_1 ? 1 : 0 ) )
+            printf( "see see " ) ;
     }
-    printf("haha");
-    return 0;
+    printf( "haha" ) ;
+    return 0 ;
 }
